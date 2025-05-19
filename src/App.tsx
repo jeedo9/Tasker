ee9dc35c179eb4dc181c09319cfc8f39ad857940
Pierre <pierrematar1912@gmail.com>
2025-05-19 11:42:28 +0200

    add react-icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { PiMoonStarsLight } from "react-icons/pi"
+
 export default function App() {
   
-  return <div className="text-amber-700 text-5xl hover:text-9xl">salut</div>
+  return <div className="text-amber-700 text-5xl hover:text-9xl">salut <PiMoonStarsLight /> </div>
 }
